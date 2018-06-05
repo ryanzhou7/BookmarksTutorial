907a147f05ca21df57b11db60a829c57a7b5922b
Ryan <Ryanzhou7@gmail.com>
2018-06-05 10:13:13 -0700

    Starter project

---
+++ src/main/java/com/ryanzhou/SpringTutBookmarksApplication.java
@@ -0,0 +1,12 @@
+package com.ryanzhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutBookmarksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutBookmarksApplication.class, args);
+	}
+}
